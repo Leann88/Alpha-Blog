c61290fea71d0a3aece818c5429b1f639b6b93b9
Leann Labelle <leann.88.labelle@gmail.com>
2017-11-15 20:19:35 -0500

    Create show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,11 +12,9 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-
   def edit
     @user = User.find(params[:id])
   end
-
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -26,6 +24,9 @@ class UsersController < ApplicationController
       flash[:danger] = "Could not update account"
       render 'edit'
     end
+  end
+  def show
+    @user = User.find(params[:id])
   end
 
   private
